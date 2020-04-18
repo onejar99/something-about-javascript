156f8b384cb941d4d36c681bc28fc0304df27de5
OneJar <onejar99@gmail.com>
2020-04-19 03:16:20 +0800

    modify gen_gitbook_docs.sh for CNAME file

--- gen_gitbook_docs.sh
+++ gen_gitbook_docs.sh
@@ -4,7 +4,10 @@ cd book-root
 docker run --rm -p 4000:4000 -v $(pwd):/gitbook onejar99/gitbook:light "gitbook install && gitbook build"
 
 cd ../
+cp docs/CNAME ./
 rm -rf docs
 cp -rf book-root/_book docs
 rm -rf docs/book_output
+cp CNAME docs/
+rm CNAME
 docker run --rm  -p 41002:8080 -v $(pwd)/docs:/home/app/public onejar99/nodejs-live-server:node12.16.1
